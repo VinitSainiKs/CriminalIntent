2b1a39d013b4a30c5ed785a6c1c2e93a5e752fc5
Mayank Wadhwa <mayankwadhwa@outlook.in>
2019-08-30 18:25:23 +0530

    august 30

---
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -0,0 +1,69 @@
+package com.example.criminalintent
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+
+class CrimeFragment: Fragment() {
+    lateinit var crime: Crime
+    lateinit var titleField: EditText
+    lateinit var dateButton: Button
+    lateinit var solvedCheckBox: CheckBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+
+        titleField = view.findViewById(R.id.crime_title)
+        dateButton = view.findViewById(R.id.crime_date)
+        solvedCheckBox = view.findViewById(R.id.crime_solved)
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val titleWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(sequence: CharSequence?, start: Int, count: Int, after: Int) {
+                // This space intentionally left blank
+            }
+
+            override fun onTextChanged(sequence: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                // This one too
+            }
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                crime.isSolved = isChecked
+            }
+        }
+    }
+
+}
